7a27d90c2ab108b798a894959ccd467ea092011b
iambekanabiev <109576462+iambekanabiev@users.noreply.github.com>
2024-08-12 01:32:35 -0500

    Delete m3problem4.py

--- m3problem4.py
+++
@@ -1,8 +0,0 @@
-# Bekzodbek
-# 08/11/2024
-# Computes the area of circle
-import math
-radius = float(input("Please enter a radius of your circle: "))
-area_of_circle = 3.14 * (radius ** 2)
-
-print("The area of your circle with radius " + str(radius) + " is " + str(area_of_circle))
